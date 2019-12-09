24bf30197b6dd50542be5f539f7453c4ff13d90d
Josip Bilandzija <josip.bilandzija@novatec-gmbh.de>
2019-12-09 07:49:20 +0100

    added logging

--- StudentService/src/main/kotlin/de/novatec/studentservice/student/StudentController.kt
+++ StudentService/src/main/kotlin/de/novatec/studentservice/student/StudentController.kt
@@ -1,5 +1,6 @@
 package de.novatec.studentservice.student
 
+import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
@@ -9,12 +10,16 @@ import java.util.*
 @CrossOrigin(origins = [], allowCredentials = "false", allowedHeaders = ["*"], methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.DELETE])
 class StudentController(private val studentRepository: StudentRepository) {
 
+    private val logger = KotlinLogging.logger {}
+
     @GetMapping("/{name}")
     fun getStudentByName(@PathVariable("name") name: String): ResponseEntity<Student> {
         val result = studentRepository.findStudentEntityByName(name)?.toStudent()
         return if (result == null) {
+            logger.info { "Get Student by name. Student not found by name: $name" }
             ResponseEntity.notFound().build()
         } else {
+            logger.info { "Get Student by name. Student found by name: $name, result: $result" }
             ResponseEntity.ok(result)
         }
     }
@@ -23,6 +28,7 @@ class StudentController(private val studentRepository: StudentRepository) {
     fun deleteStudentByName(@PathVariable("name") name: String): ResponseEntity<String> {
         val result = studentRepository.findStudentEntityByName(name)
         if (result != null) {
+            logger.info { "Delete Student by name. Student deleted by name: $name" }
             studentRepository.delete(result)
         }
         return ResponseEntity.ok(name)
@@ -32,16 +38,18 @@ class StudentController(private val studentRepository: StudentRepository) {
     fun postStudentByName(@RequestBody student: Student): ResponseEntity<Student> {
         val result = studentRepository.findStudentEntityByName(student.name)
         val temp = if (result == null) {
+            logger.info { "Create Student by name. New student created by name: $student.name" }
             studentRepository.save(StudentEntity(UUID.randomUUID().toString(), student.name))
         } else {
+            logger.info { "Update Student by name. Student updated by name: $student.name" }
             studentRepository.save(StudentEntity(result.id, student.name))
         }
         return ResponseEntity.ok(temp.toStudent())
     }
 
-    @GetMapping()
-    fun getAllCocktailIngredients(): ResponseEntity<List<Student>> {
-        println("Get All")
+    @GetMapping("/all")
+    fun getAllStudents(): ResponseEntity<List<Student>> {
+        logger.info { "Get all students." }
         return ResponseEntity.ok(studentRepository.findAll().map { it.toStudent() })
     }
 }
